06e075cf26225156aa08ef73643c9712d2b37e22
sarmsaad <sarah.msaad@yahoo.com>
2018-04-27 11:59:48 -0400

    deletes

--- app.js
+++ app.js
@@ -11,27 +11,21 @@ var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
-var mongoose = require('mongoose');
 var config = require('config');
-const {google} = require('googleapis');
 var email = null;
-// var gapi = require('gapi');
 var search = require('youtube-search');
 var path = require('path');
-var engine = require('consolidate');
-var async = require('asyncawait/async');
-var await = require('asyncawait/await');
-
-
+var mongo = require('mongodb');
 
 var client_id = config.get('client_id'); // Your client id
 var client_secret = config.get('client_secret'); // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
-let accessGlobal = "Hello";
+let accessGlobal = "";
 
 var SpotifyWebApi = require('spotify-web-api-node');
 var me = {};
+var stateKey = 'spotify_auth_state';
 
 // credentials are optional
 var spotifyApi = new SpotifyWebApi({
@@ -40,41 +34,8 @@ var spotifyApi = new SpotifyWebApi({
   redirectUri : redirect_uri
 });
 
-
-var dbConfig = config.get('dbConfig');
-mongoose.connect(dbConfig);
-
-var mongo = require('mongodb');
-
-
-var Schema = mongoose.Schema;
-var users = mongoose.model('spotify', new Schema({email: String},{collection: 'spotify'}));
-users.find({}, function(err, docs){
-  if(err)
-    console.log('error occured in the database');
-  //get just the emails
-  docs.forEach( function(element){
-     //console.log(element.email);
-  });
-});
-
-const assert = require('assert');
-
 var MongoClient = require('mongodb').MongoClient;
 var url = config.get('mongo_add');
-
-var insert = function(url){
-  MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("411");
-  var myobj = { name: "Company Inc", address: "Highway 37" };
-  dbo.collection("Users").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
-  });
-});
-}
 
 /**
  * Generates a random string containing numbers and letters
@@ -91,31 +52,12 @@ var generateRandomString = function(length) {
   return text;
 };
 
-var runSpotify = function() {
-  var search = document.getElementById("search");
-  var options = {
-    url: 'https://api.spotify.com/v1/search?type=track&q=' + search,
-    headers: { 'Authorization': 'Bearer ' + access_token },
-    json: true
-  };
-  console.log(access_token);
-  console.log(options);
-  request.get(options, function(error, response, body) {
-    document.getElementById("result").appendChild(body);
-  });
-};
-
-
-var stateKey = 'spotify_auth_state';
-
 var app = express();
 
 app.use(express.static(__dirname + '/public'))
    .use(cookieParser());
-//some html rendering thing
-// app.engine('html', engine.mustache);
-// app.set('view engine', 'html');
 
+//some html rendering thing
 app.get('/login', function(req, res) {
 
   var state = generateRandomString(16);
@@ -132,30 +74,6 @@ app.get('/login', function(req, res) {
       state: state
     }));
 });
-
-// app.get('/lol', function() {
-//   let client;
-//
-//   try {
-//     client = await MongoClient.connect(url);
-//     console.log("Connected correctly to server");
-//
-//     const db = client.db("411");
-//
-//     // Insert a single document
-//     let r = await db.collection('Users').insertOne({a:1});
-//     assert.equal(1, r.insertedCount);
-//
-//     // Insert multiple documents
-//     r = await db.collection('Users').insertMany([{a:2}, {a:3}]);
-//     assert.equal(2, r.insertedCount);
-//   } catch (err) {
-//     console.log(err.stack);
-//   }
-//
-//   // Close connection
-//   client.close();
-// });
 
 app.get('/callback', function(req, res) {
 
@@ -192,7 +110,7 @@ app.get('/callback', function(req, res) {
         var access_token = body.access_token,
             refresh_token = body.refresh_token;
 
-         accessGlobal = body.access_token;
+        accessGlobal = body.access_token;
 
         var time_range = ['short_term','medium_term','long_term'];
 
@@ -212,7 +130,7 @@ app.get('/callback', function(req, res) {
               console.log('Something went wrong!', err);
             });
 
-          //check the top artists around.
+          //check the top artists around
           var favoriteArtists = [];
           var artistPictures = [];
 
@@ -222,58 +140,46 @@ app.get('/callback', function(req, res) {
           });
 
           MongoClient.connect(url, function(err, db) {
-          if (err) throw err;
-          var dbo = db.db("411");
-          console.log(me);
-          email = me.email;
-          var myobj = { name: me.display_name, email:me.email, address: favoriteArtists, accessToken: access_token, accessed: Date.now()};
-          console.log("email" + me.email);
-          dbo.collection("Users").findOne({email: me.email}, function(err, result){
             if (err) throw err;
-            // console.log(result);
-            if (result == null){
-              console.log("adding");
-              console.log("email " + me.email);
-              dbo.collection("Users").insertOne(myobj, function(err, res) {
+            var dbo = db.db("411");
+            email = me.email;
+            var myobj = { name: me.display_name, email:me.email, address: favoriteArtists, accessToken: access_token, accessed: Date.now()};
+            var insertUpdate = function(){
+              dbo.collection("Users").findOne({email: me.email}, function(err, result){
                 if (err) throw err;
-                console.log("1 document inserted");
-                db.close();
+                if (result == null){
+                  dbo.collection("Users").insertOne(myobj, function(err, res) {
+                    if (err) throw err;
+                    console.log("1 document inserted");
+                    db.close();
+                  });
+                }else{
+                  console.log("updating")
+                  dbo.collection("Users").update(
+                      { email:me.email },
+                      { $set:
+                        {
+                          name: me.display_name,
+                          address: favoriteArtists,
+                          accessToken: access_token,
+                          accessed: Date.now()
+                        }
+                      }
+                    );
+                    db.close();
+                }
               });
-            }else{
-              console.log("updating")
-              dbo.collection("Users").update(
-                  { email:me.email },
-                  { $set:
-                    {
-                      name: me.display_name,
-                      address: favoriteArtists,
-                      accessToken: access_token,
-                      accessed: Date.now()
-                    }
-                  }
-                );
-                db.close();
             }
-          });
-/*          else{
-          dbo.collection("Users").insertOne(myobj, function(err, res) {
-            if (err) throw err;
-            console.log("1 document inserted");
-
-            db.close();
-          });
-        });*/
+          setTimeout(function(){insertUpdate()}, 500);
+        });
       });
 
-          // console.log(favoriteArtists);
-        });
-
-        // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
+      // we can also pass the token to the browser to make requests from there
+      res.redirect('/#' +
+        querystring.stringify({
+          access_token: access_token,
+          refresh_token: refresh_token
+        }));
       } else {
         res.redirect('/#' +
           querystring.stringify({
@@ -284,40 +190,14 @@ app.get('/callback', function(req, res) {
   }
 });
 
-app.get('/refresh_token', function(req, res) {
-
-  // requesting access token from refresh token
-  var refresh_token = req.query.refresh_token;
-  var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
-    },
-    json: true
-  };
-
-  request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var access_token = body.access_token;
-      res.send({
-        'access_token': access_token
-      });
-    }
-  });
-});
-
 //has q as param req aka req.query.q
 app.get('/search', function(req, res) {
-
     //setting up the search parameters
     var options = {
       url: 'https://api.spotify.com/v1/search?type=track&q=' + req.query.q +'&limit=5',
       headers: { 'Authorization': 'Bearer ' + accessGlobal },
       json: true
     };
-
     //here is where the search request is made
     request.get(options, function(error, response, body) {
       //console.log(response);
@@ -331,7 +211,6 @@ app.get('/search', function(req, res) {
         });
       }
     });
-
 });
 
 //has user_id and playlist_id as params
@@ -348,16 +227,17 @@ app.get('/playlist', function(req, res) {
     request.get(options, function(error, response, body) {
       //console.log(response);
       if (!error && response.statusCode === 200) {
-        res.send({
-          'body': body
-
-        });
         var artists = {};
         body["items"].forEach(function(arr){
+          console.log(arr);
           var name = arr["track"]["artists"][0]["name"];
+          console.log(arr["track"]);
           artists[name] = (artists[name] || 0) + 1;
         });
         console.log(artists);
+        res.send({
+          'artists': artists;
+        });
       }else{
         res.send({
           'body': 'something went wrong'
@@ -371,7 +251,6 @@ app.get('/playlist', function(req, res) {
 app.get('/youtube', function(req, res){
   var list = [];
   var vids = [];
-  var error = 'yes';
   var x = function (){
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
@@ -381,7 +260,6 @@ app.get('/youtube', function(req, res){
         if (result == null){
           console.log("coudn't find the logged in user's data");
         }else{
-          console.log(result);
           //THE ARRAY OF ARTISTS HERE
           list = result.address;
           console.log("the list: ", list);
@@ -393,27 +271,21 @@ app.get('/youtube', function(req, res){
             search(element+ ' live concert', opts, function(err, results) {
               if(err) return console.log(err);
               vids.push(results[0].id);
-              console.log(results[0].id);
             });
           });
-          if(vids.length != 0){
-            error = 'no';
-          }
         }
         db.close();
       });
     });
-    return null;
   };
   x();
   var please = function(){
     console.log('sending the request');
     res.send({
-      'ids': vids,
-      'error': error
+      'ids': vids
     });
   };
-  setTimeout(function(){ please()}, 500);
+  setTimeout(function(){please()}, 500);
 });
 
 
@@ -463,9 +335,13 @@ app.get('/playlistthing', function(req, res){
 });
 
 app.get("/yt", (req, res) => {
-   res.sendfile('./public/youtube.html', {hello:"HELLO"});
+  console.log("HERESS:");
+  res.render('youtube', {hello: "asdfasdfasdf"});
 });
 
+app.get('/', function (req, res) {
+  res.render('home', {title: "asdfasdfasdf"});
+});
 
 console.log('Listening on 8888');
 app.listen(8888);
